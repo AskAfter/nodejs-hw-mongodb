75626ea1eff2effdc809eb16f1413ccf7b310ac0
AskAfter <askafter904@gmail.com>
2025-05-11 02:14:13 +0200

    swagger done

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { logger } from './middlewares/logger.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export const setupServer = () => {
   const app = express();
@@ -16,6 +17,7 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(logger);
 
